2a8cb9eafbca28903163cbd59c2352e13c21ecf4
Samuele Labate <111309193+DentArthur1@users.noreply.github.com>
2025-03-31 13:40:48 +0200

    Ora le formule scritte sono visibili ma solo dopo essere usciti dal processo di scrittura

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import TestoDiProva  # Importa il modello
+from django.utils.safestring import mark_safe
+from .models import TestoDiProva
 
-# Registra il modello
-admin.site.register(TestoDiProva)
+class TestoDiProvaAdmin(admin.ModelAdmin):
+    list_display = ('titolo', 'render_contenuto')
+
+    def render_contenuto(self, obj):
+        # Usa MathJax per il rendering delle formule
+        return mark_safe(f"""
+            <div>{obj.contenuto}</div>
+            <script type="text/javascript" async
+                src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+            </script>
+            <script>
+                MathJax.typesetPromise();  // Forza MathJax a eseguire il rendering
+            </script>
+        """)
+
+    render_contenuto.short_description = "Contenuto (renderizzato)"
+
+# Registra il modello con la configurazione personalizzata
+admin.site.register(TestoDiProva, TestoDiProvaAdmin)
